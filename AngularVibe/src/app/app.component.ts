5f8ed236d0c4ca1b13210df1df854424393d830c
LindaMangena <LindaJazz42@gmail.com>
2021-05-22 11:49:24 -0400

    Binding text using ternary operator

--- AngularVibe/src/app/app.component.ts
+++ AngularVibe/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularVibe';
+  title = 'Style Binding';
+  public hasError = false;
+  public isSpecial = true;
+
 }
